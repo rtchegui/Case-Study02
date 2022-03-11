698a647d3588c0306ff28c8cffec16a3757ec779
Roger Tchegui <tchegui.roger226@gmail.com>
2022-03-10 23:42:06 -0600

    visualization code

---
+++ Visualization_Assignments/sketch.js
@@ -0,0 +1,16 @@
+var a = 0;
+
+function setup() { 
+  createCanvas(400, 400);
+
+} 
+
+function draw() { 
+  background(220);
+  rectMode(CENTER);
+  translate(width/2, height/2);
+  rotate(a);
+  rect(0, 0, width/2, height/4);
+  line(0, 0, 0, height/8);
+  a = a + 0.01
+}
